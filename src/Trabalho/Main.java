d2297fdecc20f0e94b20d21f9141dd2d72c0e38a
Antônio Carlos <juniorbecari@gmail.com>
2022-10-11 11:22:28 -0300

    Now it flushes the screen

--- src/Trabalho/Main.java
+++ src/Trabalho/Main.java
@@ -10,6 +10,8 @@ public class Main {
         Estoque est = new Estoque();
         
         while (true) {
+            clear();
+            
             System.out.println("Cofrinho");
             System.out.println("Voce tem R$ " + est.getReais() + "\n");
             
@@ -25,8 +27,14 @@ public class Main {
             switch (opcao) {
                 case 0:
                     System.exit(0);
+                    break;
                 
                 case 1:
+                    clear();
+                    
+                    System.out.println("Cofrinho");
+                    System.out.println("Voce tem R$ " + est.getReais() + "\n");
+                    
                     System.out.println("Selecione uma moeda para adicionar:\n");
                     
                     System.out.println("1 - Real");
@@ -36,16 +44,24 @@ public class Main {
                     System.out.print("> ");
                     int moeda = s.nextInt();
                     
-                    System.out.println("Qual o valor?");
+                    clear();
+                    
+                    System.out.println("Cofrinho");
+                    System.out.println("Voce tem R$ " + est.getReais() + "\n");
+                    
+                    System.out.println("Qual o valor?\n");
                     System.out.print("> ");
                     double valor = s.nextDouble();
                     
                     est.adicionar(valor, moeda);
-                    System.out.println("-----------");
-                    
                     break;
                
                case 2:
+                    clear();
+                    
+                    System.out.println("Cofrinho");
+                    System.out.println("Voce tem R$ " + est.getReais() + "\n");
+                    
                     System.out.println("Selecione uma moeda para remover:\n");
                     
                     System.out.println("1 - Real");
@@ -55,16 +71,25 @@ public class Main {
                     System.out.print("> ");
                     moeda = s.nextInt();
                     
-                    System.out.println("Qual o valor?");
+                    clear();
+                    
+                    System.out.println("Cofrinho");
+                    System.out.println("Voce tem R$ " + est.getReais() + "\n");
+                    
+                    System.out.println("Qual o valor?\n");
                     System.out.print("> ");
                     valor = s.nextDouble();
                     
                     est.remover(valor, moeda);
-                    System.out.println("-----------");
                     
                     break;
                 
                 case 3:
+                    clear();
+                    
+                    System.out.println("Cofrinho");
+                    System.out.println("Voce tem R$ " + est.getReais() + "\n");
+                    
                     System.out.println("O que deseja converter?\n");
                     
                     System.out.println("1 - Seu saldo (R$ " + est.getReais() + ")");
@@ -73,26 +98,40 @@ public class Main {
                     System.out.print("> ");
                     int op = s.nextInt();
                     
+                    clear();
+                    
+                    System.out.println("Cofrinho");
+                    System.out.println("Voce tem R$ " + est.getReais() + "\n");
+                    
                     switch (op) {
                         case 1:
                             System.out.println("Em Dolar: " + est.realParaDolar(est.getReais()));
                             System.out.println("Em Euro: " + est.realParaEuro(est.getReais()));
-                            
-                            System.out.println("-----------");
                             break;
                         case 2:
-                            System.out.println("Qual o valor (Em Reais)?");
+                            System.out.println("Qual o valor (Em Reais)?\n");
                             System.out.print("> ");
                             valor = s.nextDouble();
                             
                             System.out.println("Em Dolar: " + est.realParaDolar(valor));
                             System.out.println("Em Euro: " + est.realParaEuro(valor));
-                            
-                            System.out.println("-----------");
                             break;
                     }
+                    
+                    System.out.println("\nPressione Enter para continuar");
+                    
+                    try {
+                        System.in.read();
+                    } catch (Exception e) {}
+                    
+                    break;
             }
-            
         }
+    }
+    
+    public static void clear() {
+        // Sim, isso realmente limpa a tela
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
