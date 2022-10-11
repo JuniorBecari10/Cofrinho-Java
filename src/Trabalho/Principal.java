cedeccfb2f2c82d25a8b3733a67a7819e6f962c2
Antônio Carlos <juniorbecari@gmail.com>
2022-10-11 09:20:55 -0300

    Created class Estoque

--- src/Trabalho/Principal.java
+++ src/Trabalho/Principal.java
@@ -1,8 +1,20 @@
 package Trabalho;
 
+import java.util.Scanner;
+
 public class Principal {
     
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int opcao;
         
+        System.out.println("Cofrinho");
+        System.out.println("1 - Adicionar Moeda");
+        System.out.println("2 - Remover Moeda");
+        System.out.println("3 - Listagens de Moedas");
+        System.out.println("4 - Converter Moeda");
+        System.out.println("0 - Encerrar");
+        
+        opcao = teclado.nextInt();
     }
 }
